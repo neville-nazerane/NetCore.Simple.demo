485fcdc920600cc9f37c8320467d01d0f5e5f616
Neville Nazerane <live.neville@live.com>
2017-09-16 20:05:40 -0400

    basic functionalities updated

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,37 @@
+﻿using Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+using NetCore.Simple;
+using NetCore.Simple.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Controllers
+{
+    [Route("person/{id?}")]
+    public class PersonController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index(Person person) {
+            var editor = new Editor();
+            if (person.Get()) {
+                editor.Data = person;
+            }
+            return View(editor);
+        }
+
+        [HttpPost]
+        [ValidateModel]
+        public void Add(Person person) => person.Add();
+
+        [HttpPut]
+        [ValidateModel]
+        public void Update(Person person) => person.Update();
+
+        [HttpDelete]
+        public void Delete(Person person) => person.Delete();
+
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Models
+{
+    public class Person
+    {
+        private static List<Person> _Data = new List<Person>();
+
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [MinLength(4)]
+        public string FirstName { get; set; }
+
+        [MaxLength(60)]
+        public string LastName { get; set; }
+
+        public void Add() {
+            ID = _Data.Count() + 1;
+            _Data.Add(this);
+        }
+
+        public bool Update() {
+            var data = _Data.SingleOrDefault(p => p.ID == ID);
+            if (data == null) return false;
+            data.FirstName = FirstName;
+            data.LastName = LastName;
+            return true;
+        }
+
+        public bool Get() {
+            var data = _Data.SingleOrDefault(p => p.ID == ID);
+            if (data == null) return false;
+            FirstName = data.FirstName;
+            LastName = data.LastName;
+            return true;
+        }
+
+        public bool Delete() {
+            var data = _Data.SingleOrDefault(p => p.ID == ID);
+            if (data == null) return false;
+            _Data.Remove(data);
+            return true;
+        }
+
+        public static List<Person> GetAll()
+        {
+            return _Data;
+        }
+
+    }
+}
